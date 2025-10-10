2420b449126f765ba8e5180e65e415a18ec876b6
Wallysson <118409802+WallyssonSousa@users.noreply.github.com>
2025-10-10 18:24:26 -0300

    Feat: Adicionando função para o login funcionar tanto para funcionário, admin e para cliente.

--- routes/LoginRoute.py
+++ routes/LoginRoute.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, JWTManager
-import os
+from werkzeug.security import check_password_hash
 from datetime import timedelta
+import os
+
+from database import db
+from models.ClienteModel import Cliente  
 
 login_bp = Blueprint("login", __name__)
 
@@ -9,12 +13,13 @@ ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "admin")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin123")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "defaultsecret")
 
-# Criação do JWT
+
 def init_jwt(app):
     app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1) # tokem dura por uma hora
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     JWTManager(app)
-    
+
+
 @login_bp.route("/login", methods=["POST"])
 def login():
     dados = request.get_json()
@@ -27,6 +32,29 @@ def login():
             identity=username,
             additional_claims={"role": role}
         )
-        return jsonify({"mensagem": "Login realizado com sucesso", "token": token, "role": role}), 200
-    
+        return jsonify({
+            "mensagem": "Login realizado com sucesso (admin)",
+            "token": token,
+            "role": role
+        }), 200
+
+    cliente = Cliente.query.filter_by(email=username).first()
+
+    if cliente and cliente.verificar_senha(password):
+        role = "cliente"
+        token = create_access_token(
+            identity=cliente.email,
+            additional_claims={"role": role, "id": cliente.id}
+        )
+        return jsonify({
+            "mensagem": "Login realizado com sucesso (cliente)",
+            "token": token,
+            "role": role,
+            "cliente": {
+                "id": cliente.id,
+                "nome": cliente.nome,
+                "email": cliente.email
+            }
+        }), 200
+
     return jsonify({"erro": "Credenciais inválidas"}), 401
